49483fdbe4ac3fc44bb9d6c81b8c6415b7b3fa20
Jenish-Hariyani <150036720+4072-jenish@users.noreply.github.com>
2026-03-02 15:24:18 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,4 +16,4 @@ app.use(
 );
 app.use('/', indexRouter);
 
-app.listen(PORT , () => console.log(`Server is running on http://localhost:${PORT}`));
+app.listen(PORT , "0.0.0.0", () => console.log(`Server is running on http://localhost:${PORT}`));
