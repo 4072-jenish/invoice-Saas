8d8b31be624f0e8b3e2d47ebec5ac358444b0fa4
Jenish-Hariyani <150036720+4072-jenish@users.noreply.github.com>
2026-03-09 11:54:15 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://invoice-saas-redeploed.vercel.app/",
     credentials: true, 
   })
 );
