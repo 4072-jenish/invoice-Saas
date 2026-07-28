bf61bb3f59cdef758f05220b95dfa74e90f11f58
Jenish Hariyani <hariyanijenish@gmail.com>
2026-07-28 10:17:09 +0530

    cors updated

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(
   cors({
-    origin: "https://invoice-saas-redeploed.vercel.app",
+    origin: "https://invoice-saas-redeploed.vercel.app"
+            "http://localhost:5173",
     credentials: true, 
   })
 );
